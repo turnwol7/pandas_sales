42077b0dc07299a028665c3e2bb36fd35d0db409
turnwol7 <justinb.developer@gmail.com>
2025-03-25 11:28:36 -0300

    plotting

--- script.py
+++ script.py
@@ -1,5 +1,34 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('sales_data.csv')
 
 print(data.head())
+print("missing values:\n", data.isnull().sum())
+
+data["Date"] = pd.to_datetime(data["Date"])
+
+data = data.drop_duplicates()
+
+total_revenue = data["Revenue"].sum()
+
+total_units = data["Units_Sold"].sum()
+print(f"total revenue is {total_revenue}")
+print(f"total units sold is {total_units}")
+
+best_selling_product = data.groupby("Product")["Revenue"].sum().idxmax()
+print(f"Best-Selling Product: {best_selling_product}")
+
+average_revenue = data.groupby("Product")["Revenue"].mean()
+print(f"average revenue per product: {average_revenue}")
+
+#plotting
+
+plt.figure(figsize=(10, 5))
+plt.plot(data["Date"], data["Revenue"], marker="o", linestyle="-", color="b")
+plt.xlabel("Date")
+plt.ylabel("Revenue ($)")
+plt.title("Daily Revenue Trend")
+plt.xticks(rotation=45)
+plt.grid()
+plt.show()
